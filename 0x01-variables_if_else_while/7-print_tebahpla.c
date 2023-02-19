a58176890e41ff1fc068efbecbaf4fcbe4707c86
maryamararomi <araromimaryam69@gmail.com>
2023-02-19 03:57:17 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - Print the lower case alphabets in reverse
+  * Return: Always 0
+  */
+int main(void)
+{
+	char first;
+
+	first = 'z';
+	while (first <= 'a')
+	{
+		putchar(first);
+		first = first + 1;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
