cf7aedfa2b45cce347ed2397b737cd1122c67d7a
maryamararomi <araromimaryam69@gmail.com>
2023-03-06 13:26:56 -0800

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _memcpy - a function tat copies memory area
+  * @dest: dest memory area
+  * @src: source memory area
+  * @n: byte from memory area to copy
+  * Return: a pointer to dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
+	
